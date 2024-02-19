7dd853ebf3ee894adc08264f076f8ab1d6acccd5
Samuel Braniste <samuel.braniste@gmail.com>
2024-02-19 14:35:06 +0100

    stupid coaching

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get "/ask", to: "questions#ask"
+  get "/answer", to: "questions#answer"
+  post "/answer", to: "questions#answer"
+
+
+
+ #faut-il rajouter le lien de l'url ???
 end
